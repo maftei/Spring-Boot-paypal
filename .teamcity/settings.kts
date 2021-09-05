572faf79deb7506020790b90f4a57d19935708a0
razvan <razvan.maftei@yahoo.com>
2021-09-05 18:55:01 +0300

    new settings file

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,3 +1,4 @@
+import jetbrains.buildServer.configs.kotlin.v10.toExtId
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.freeDiskSpace
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
@@ -57,6 +58,7 @@ project {
 
 
 class Maven(name: String, goals: String, runnerArgs: String? = null): BuildType({
+    id(name.toExtId())
     this.name=name
 
     vcs {
